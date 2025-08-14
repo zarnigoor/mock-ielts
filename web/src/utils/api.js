558dd10eccbd1aeceb9e75827d555fd741aa69b7
Zarnigoor <zarnigorumaraliyeva8@gmail.com>
2025-08-14 11:41:31 -0700

    added backend deployment url

--- web/src/utils/api.js
+++ web/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001';
+const API_BASE_URL = 'https://mock-ielts.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
